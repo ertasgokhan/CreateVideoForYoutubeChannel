f78b28c92535b9cecb94ec56f94e363224a91084
Gökhan Ertaş <gokhan.ertas@outlook.com>
2022-01-24 17:25:11 +0300

    Add project files.

---
+++ CreateVideoForYoutubeChannel/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using CreateVideoForYoutubeChannel.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace CreateVideoForYoutubeChannel.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new YuotubeModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(YuotubeModel model)
+        {
+            string urlAddress = model.Url;
+
+            string result = "";
+            HttpWebRequest httpRequest = (HttpWebRequest)HttpWebRequest.Create(urlAddress);
+            httpRequest.Timeout = 10000;
+            httpRequest.UserAgent = "Code Sample Web Client";
+            httpRequest.Credentials = CredentialCache.DefaultCredentials;
+            HttpWebResponse respone = (HttpWebResponse)httpRequest.GetResponse();
+            StreamReader stream = new StreamReader(respone.GetResponseStream(), Encoding.UTF8);
+            result = stream.ReadToEnd();
+
+
+            string filepath = @"C:\Yutup\" + model.ProductName.Replace(" ", "").Trim() + ".txt";
+
+            if (System.IO.File.Exists(filepath))
+                System.IO.File.Delete(filepath);
+
+            using (StreamWriter sw = System.IO.File.CreateText(filepath))
+            {
+                sw.WriteLine(result);
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CreateVideoForYoutubeChannel/Models/YuotubeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreateVideoForYoutubeChannel.Models
+{
+    public class YuotubeModel
+    {
+        public string ProductName { get; set; }
+        public string FolderName { get; set; }
+        public string Url { get; set; }
+    }
+}
