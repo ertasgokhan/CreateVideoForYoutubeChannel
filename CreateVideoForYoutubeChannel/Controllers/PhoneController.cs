91f74167854272292db103df27666af079b416db
Gökhan Ertaş <gokhan.ertas@outlook.com>
2022-02-27 20:10:05 +0300

    NawBar and PowerBank Page added

--- CreateVideoForYoutubeChannel/Controllers/PhoneController.cs
+++ CreateVideoForYoutubeChannel/Controllers/PhoneController.cs
@@ -1,10 +1,13 @@
 ﻿using CreateVideoForYoutubeChannel.Models;
 using DocumentFormat.OpenXml.Packaging;
+using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace CreateVideoForYoutubeChannel.Controllers
@@ -13,7 +16,87 @@ namespace CreateVideoForYoutubeChannel.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var model = new YuotubeModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(YuotubeModel model)
+        {
+            // Epey
+            string result = "";
+            string docItems = "";
+
+            HttpWebRequest httpRequest = (HttpWebRequest)HttpWebRequest.Create(model.Url);
+            httpRequest.Timeout = 10000;
+            httpRequest.UserAgent = "Code Sample Web Client";
+            httpRequest.Credentials = CredentialCache.DefaultCredentials;
+            HttpWebResponse respone = (HttpWebResponse)httpRequest.GetResponse();
+            StreamReader stream = new StreamReader(respone.GetResponseStream(), Encoding.UTF8);
+            result = stream.ReadToEnd();
+
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(result);
+
+            HtmlNode productName = doc.DocumentNode.SelectSingleNode("//div[@class='baslik']//h1//a");
+            model.ProductName = productName.InnerText;
+
+            string filepath = @"C:\Yutup\PHONE\" + model.ProductName.Replace(" ", "").Replace("/", "").Trim() + ".txt";
+
+            if (System.IO.File.Exists(filepath))
+                System.IO.File.Delete(filepath);
+
+            using (StreamWriter sw = System.IO.File.CreateText(filepath))
+            {
+                docItems += string.Format("Ürün->{0} \n", model.ProductName);
+                docItems += string.Format("Url->{0} \n", model.Url);
+
+                HtmlNode properties = doc.DocumentNode.SelectSingleNode("//div[@id='bilgiler']");
+
+                if (properties != null)
+                {
+                    HtmlNodeCollection groups = doc.DocumentNode.SelectNodes(".//div[@id='grup']");
+
+                    if (groups != null && groups.Count > 0)
+                    {
+                        string groupName = string.Empty;
+
+                        for (int i = 0; i < groups.Count; i++)
+                        {
+                            HtmlNode newItem = groups[i];
+
+                            groupName = newItem.SelectSingleNode(".//h3").InnerText;
+
+                            docItems += string.Format("***{0}**** \n", groupName);
+
+                            HtmlNode groupProperty = newItem.SelectSingleNode(".//ul[@class='grup']");
+
+                            HtmlNodeCollection groupChilNodes = groupProperty.ChildNodes;
+
+                            string propertyName = string.Empty;
+                            string propertyValue = string.Empty;
+
+                            foreach (var item2 in groupChilNodes)
+                            {
+                                if (item2.Name == "li")
+                                {
+                                    propertyName = item2.SelectSingleNode("strong").InnerText;
+                                    propertyValue = item2.SelectSingleNode("span//a") != null ? item2.SelectSingleNode("span//a").InnerText.Replace("\n", "") : item2.SelectSingleNode("span//span").InnerText.Replace("\n", "");
+
+                                    docItems += string.Format("{0}->{1} \n", propertyName, propertyValue);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                sw.WriteLine(docItems);
+            }
+
+            ViewData["Message"] = "Dosya Başarıyla Oluşturulmuştur";
+
+            return View(model);
         }
 
         public IActionResult PhoneVS()
@@ -73,7 +156,7 @@ namespace CreateVideoForYoutubeChannel.Controllers
 
                         // change the text
                         if (text != null)
-                            text.Text = phone1Properties.FirstOrDefault(x => x.Contains(item)).Split("->")[1].Replace("Var", "Yes").Replace("Yok", "No").Replace("Milyon", "Million").Replace("Milyar", "Billion").Replace("Cam", "Glass").Replace("Alüminyum", "Aluminum").Replace("Gram", "Grams").Replace("Çekirdek", "Core").Replace("Kablosu","Cable").Replace("Piksel","Pixel").Replace("Çift Hat", "Dual SIM").Replace("Ocak", "January").Replace("Şubat", "February").Replace("Mart", "March").Replace("Nisan", "April").Replace("Mayıs", "May").Replace("Haziran", "June").Replace("Temmuz", "July").Replace("Ağustos", "August").Replace("Eylül", "September").Replace("Ekim", "October").Replace("Kasım", "November").Replace("Aralık", "December").Replace("Depolama seçeneği var", "Storage Option").Replace("Paslanmaz Çelik", "Non Rusting Steel").Replace("Çift Batarya", "Dual Battery").Replace("seçeneği var", "Option").Replace("Ekran İçinde", "Under-Display").Replace("Tek Hat","Single SIM").Replace("İnç", "Inch");
+                            text.Text = phone1Properties.FirstOrDefault(x => x.Contains(item)).Split("->")[1].Replace("Var", "Yes").Replace("Yok", "No").Replace("Milyon", "Million").Replace("Milyar", "Billion").Replace("Cam", "Glass").Replace("Alüminyum", "Aluminum").Replace("Gram", "Grams").Replace("Çekirdek", "Core").Replace("Kablosu", "Cable").Replace("Piksel", "Pixel").Replace("Çift Hat", "Dual SIM").Replace("Ocak", "January").Replace("Şubat", "February").Replace("Mart", "March").Replace("Nisan", "April").Replace("Mayıs", "May").Replace("Haziran", "June").Replace("Temmuz", "July").Replace("Ağustos", "August").Replace("Eylül", "September").Replace("Ekim", "October").Replace("Kasım", "November").Replace("Aralık", "December").Replace("Depolama seçeneği var", "Storage Option").Replace("Paslanmaz Çelik", "Non Rusting Steel").Replace("Çift Batarya", "Dual Battery").Replace("seçeneği var", "Option").Replace("Ekran İçinde", "Under-Display").Replace("Tek Hat", "Single SIM").Replace("İnç", "Inch");
                     }
                 }
 
