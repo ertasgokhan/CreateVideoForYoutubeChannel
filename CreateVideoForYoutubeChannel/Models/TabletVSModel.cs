5b7c606a4210e9873a1dc935e099e64e9d8350ab
Gökhan Ertaş <gokhan.ertas@outlook.com>
2022-05-08 23:06:28 +0300

    Tablet is added

---
+++ CreateVideoForYoutubeChannel/Models/TabletVSModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreateVideoForYoutubeChannel.Models
+{
+    public class TabletVSModel
+    {
+        public string Tablet1 { get; set; }
+
+        public string Tablet2 { get; set; }
+    }
+}
